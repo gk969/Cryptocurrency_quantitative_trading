3dc93a89ad6a1b7cf6d675646c7e9e1440ae9794
SongJian <admin@gk969.com>
2021-06-13 01:08:32 +0800

    get kline data ok

--- main.py
+++ main.py
@@ -6,33 +6,118 @@ import json
 import gzip
 from enum import Enum
 
+TIME_FORMAT = "%Y-%m-%d %H:%M:%S"
+
+KLINE_CH = "market.ethusdt.kline.1min"
+KLINE_PERIOD = 60
+
 kline_sub = {
-    "sub": "market.ethusdt.kline.1min",
+    "sub": KLINE_CH,
     "id": "id1"
 }
 
-State = Enum('State', ('IDLE', 'SUB', 'REQ'))
+State = Enum('State', ('IDLE', 'SUB', 'REQ', 'END'))
 state = State.IDLE
+MAX_KLINE_PER_REQ = 720
+# REQ_KLINE_START = "2017-11-01 00:00:00"
+REQ_KLINE_START = "2021-06-01 12:00:00"
+REQ_KLINE_START = int(time.mktime(time.strptime(REQ_KLINE_START, TIME_FORMAT)))
+REQ_KLINE_END = "2021-06-12 23:00:00"
+REQ_KLINE_END = int(time.mktime(time.strptime(REQ_KLINE_END, TIME_FORMAT)))
+
+kline_req_time = REQ_KLINE_START
+kline_send_time = time.time()
+
+
+def send_kline_req(ws):
+    global kline_send_time
+    to_time = min(REQ_KLINE_END, kline_req_time + KLINE_PERIOD * (MAX_KLINE_PER_REQ - 1))
+    kline_req = {
+        "req": KLINE_CH,
+        "id": "req " + str(kline_req_time),
+        "from": kline_req_time,
+        "to": to_time
+    }
+    print('req kline %s' % time.strftime(TIME_FORMAT, time.localtime(kline_req_time)))
+    ws.send(json.dumps(kline_req))
+    kline_send_time = time.time()
+
+
+# {
+#     "id": "req 1623470400",
+#     "status": "ok",
+#     "ts": 1623515792624,
+#     "rep": "market.ethusdt.kline.1min",
+#     "data": [
+#         {
+#             "id": 1623470400,
+#             "open": 2300.51,
+#             "close": 2306.87,
+#             "low": 2296.58,
+#             "high": 2307.39,
+#             "amount": 559.162769977232,
+#             "vol": 1287419.0268039096,
+#             "count": 1268
+#         }
+#     ]
+# }
+
+
+def is_kline_data_valid(data):
+    global kline_req_time
+    if data[0].get('id') != kline_req_time:
+        print('kline start error')
+        return False
+
+    for i in range(len(data)):
+        item = data[i]
+        if not ('id' in item and 'open' in item and 'close' in item and 'low' in item and 'high' in item and
+                'amount' in item and 'vol' in item and 'count' in item):
+            print('kline key error')
+            return False
+
+        if item.get('id') != (kline_req_time + i * KLINE_PERIOD):
+            print('kline id error')
+            return False
+
+    return True
 
 
 def on_message(ws, msg):
     msg = gzip.decompress(msg).decode('utf8')
     msg = json.loads(msg)
-    global state
-    print(f"on_message {msg}")
+    global state, kline_req_time
+    # print(f"on_message {msg}")
     if "ping" in msg:
         pong = {"pong": msg['ping']}
         ws.send(json.dumps(pong))
 
     if state == State.SUB:
-        if 'id' in msg and 'subbed' in msg and 'status' in msg:
-            if msg['id'] == kline_sub['id'] and msg['subbed'] == kline_sub['sub'] and msg['status'] == 'ok':
-                state = State.REQ
-
-                return
-            else:
-                print(f"sub kline error : {msg}")
+        if msg.get('id') == kline_sub['id'] and msg.get('subbed') == kline_sub['sub'] and msg.get('status') == 'ok':
+            state = State.REQ
+            return
+        else:
+            print(f"sub kline error : {msg}")
         ws.send(json.dumps(kline_sub))
+
+    if state == State.REQ:
+        if msg.get('id') == ("req " + str(kline_req_time)) and msg.get('status') == 'ok' and \
+                msg.get('rep') == KLINE_CH and type(msg.get('data')) == list:
+            # print(f"{msg}")
+            data = msg.get('data')
+            print(f'data len {len(data)}')
+            if is_kline_data_valid(data):
+                kline_req_time += len(data) * KLINE_PERIOD
+                if kline_req_time >= REQ_KLINE_END:
+                    state = State.END
+                    print('kline req end')
+                    return
+            time.sleep(0.11)
+            send_kline_req(ws)
+        else:
+            cur_time = time.time()
+            if cur_time - kline_send_time >= 1:
+                send_kline_req(ws)
 
 
 def on_error(ws, error):
@@ -51,15 +136,15 @@ def on_open(ws):
     ws.send(json.dumps(kline_sub))
 
 
-# websocket.enableTrace(True)
-ws = websocket.WebSocketApp("wss://api.huobi.pro/ws",
-                            on_open=on_open,
-                            on_message=on_message,
-                            on_error=on_error,
-                            on_close=on_close)
+def start_web_socket():
+    # websocket.enableTrace(True)
+    ws = websocket.WebSocketApp("wss://api.huobi.pro/ws",
+                                on_open=on_open,
+                                on_message=on_message,
+                                on_error=on_error,
+                                on_close=on_close)
 
-ws.run_forever(http_proxy_host="127.0.0.1", http_proxy_port=1081)
+    ws.run_forever(http_proxy_host="127.0.0.1", http_proxy_port=1081)
 
-# from_time = "2017-11-00 00:00:00"
-# kline_req = requests.get('https://api.hbdm.com/linear-swap-ex/market/history/kline',
-#                          params={'q': 'python', 'cat': '1001'}, timeout=5)
+
+start_web_socket()
